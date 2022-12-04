8953620f05d7445a60f656bee4c9fa88a189a1b5
Barash777 <barash.vertihvost@gmail.com>
2022-12-05 00:26:31 +0300

    added dispatch error=null

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, {AlertProps} from '@mui/material/Alert';
-import {useAppSelector} from '../../hooks/hooks';
+import {useAppDispatch, useAppSelector} from '../../hooks/hooks';
+import {appSetErrorAC} from '../../app/app-reducer';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -9,15 +10,17 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props,
 
 export const CustomizedSnackbars = () => {
     // const [open, setOpen] = React.useState(true);
+    const error = useAppSelector(state => state.app.error)
+    const dispatch = useAppDispatch()
 
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
             return;
         }
-        // setOpen(false);
+        dispatch(appSetErrorAC(null))
+        // setOpen(false)
     };
 
-    const error = useAppSelector(state => state.app.error)
     const isOpen = error !== null
 
     return (
